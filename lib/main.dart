54f16a1968b6aff87bb9afe82b9f01150be0fb5d
SandroCampoGit <141502362+SandroCampoGit@users.noreply.github.com>
2023-11-22 15:26:30 +0200

    Complete connection to Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/EventBooking.dart';
 
+import 'firebase_options.dart';
 
-void main() {
+
+Future<void> main() async {
   runApp(MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
 }
 
 class MyApp extends StatelessWidget {
